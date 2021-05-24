b16f43bdee6d8a14b6c7c1a3018d1e1ab91cb9a0
Konstantinos Koupepas <kk1219@ic.ac.uk>
2021-05-24 15:30:18 +0100

    Implemented freeMemory in memory.h

--- src/memory.h
+++ src/memory.h
@@ -5,8 +5,12 @@
 
 extern int8_t *memory;
 
+void intitialize(void);
+
 int store(uint16_t address, int8_t data);
 
 uint32_t load(uint16_t address, int num_bytes);
+
+void freeMemory(void);
 
 #endif
