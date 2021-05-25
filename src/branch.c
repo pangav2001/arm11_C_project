5834ba07cb3856ab47345027c889ccede989a017
Naman Sharma <nns20@ic.ac.uk>
2021-05-25 19:40:01 +0100

    branch.c done

--- src/branch.c
+++ src/branch.c
@@ -4,11 +4,17 @@
 #include <assert.h>
 
 
+
 void branch(int offset){
     // This offset is shifted left 2 bits, sign extended
     //to 32 bits and then added to the PC
 
-    int temp = offset << 2;
-    //temp = 
+    //assert(offset != NULL); want to assert 24 bits in length
+
+    int32_t temp = (int32_t) offset << 2;
+    
     // PC is 8 bytes ahead of the instruction that is being executed
+    int32_t pc_value = get_reg(PC);
+    pc_value  = (pc_value -8) + temp;
+    store_reg(PC,pc_value);
 }
