42f7d2675c95c21a636d13619dfed5e733572307
Karim Selih <ks1020@ic.ac.uk>
2021-05-25 15:58:47 +0100

    added flags header

--- src/flags.h
+++ src/flags.h
@@ -1,2 +1,25 @@
 #include <stdint.h>
 
+enum Condition_Codes
+{
+    EQ,
+    NE,
+    GE,
+    LT,
+    GT,
+    LE,
+    AL
+};
+
+enum Flags {
+    N,
+    Z,
+    C,
+    V
+};
+
+int check_cond(enum Condition_Code);
+
+void set_flag(enum Flags flag);
+
+void reset_flag(enum Flags flag);
