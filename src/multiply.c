22161fd2a9f30128489fd6b9ce63b78f717c1ed6
Naman Sharma <nns20@ic.ac.uk>
2021-05-25 20:44:32 +0100

    multiply w/o N flag

---
+++ src/multiply.c
@@ -0,0 +1,27 @@
+#include <stdint.h>
+#include "multiply.h"
+#include "registers.h"
+#include "flags.h"
+
+
+void multiply(int A, int S,
+ Register_Names Rd ,Register_Names Rn, Register_Names Rs, Register_Names Rm){
+
+    int32_t result = get_reg(Rm) * get_reg(Rs);
+
+    if(A == 1){
+        result = result + get_reg(Rn);
+    }
+
+    store_reg(Rd,result);
+
+    if(S == 1){
+        if(result == 0){
+            set_flag(Z);
+        }
+        // N flag is set to last bit of result -- define get last bit func
+    }
+
+ }
+
+
