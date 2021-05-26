645bfa6eedfc9336499167a65e974256e58140ce
Naman Sharma <nns20@ic.ac.uk>
2021-05-26 11:42:46 +0100

    added assert to mutiply

--- src/branch.c
+++ src/branch.c
@@ -2,7 +2,7 @@
 #include "branch.h"
 #include "registers.h"
 #include <assert.h>
-
+#include <stdio.h>
 
 
 void branch(int offset){
@@ -12,9 +12,22 @@ void branch(int offset){
     //assert(offset != NULL); want to assert 24 bits in length
 
     int32_t temp = (int32_t) offset << 2;
+    printf("value %d \n", temp);
     
     // PC is 8 bytes ahead of the instruction that is being executed
     int32_t pc_value = get_reg(PC);
-    pc_value  = (pc_value -8) + temp;
+    pc_value  = (pc_value - 8) + temp;
     store_reg(PC,pc_value);
 }
+
+
+/* For testing
+int main(void){
+
+    long int input = 1110 0110 1001 1001 1001 1000;
+
+    branch(input);
+
+    return 0;
+}
+*/
