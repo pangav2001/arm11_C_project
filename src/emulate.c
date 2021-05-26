e081e6c8b110cead1e8d833709c02f55e358d85a
Konstantinos Koupepas <kk1219@ic.ac.uk>
2021-05-26 18:28:46 +0100

    [kk1219] - fixed bugs in data_processing.c

--- src/emulate.c
+++ src/emulate.c
@@ -4,30 +4,37 @@
 #include "registers.h"
 #include "parse_file.h"
 #include "decode.h"
+#include "data_processing.h"
 
 
 int main(int argc, char **argv) {
-  //init memory and reg
-  initialize_memory();
-  initialize_registers();
+  // //init memory and reg
+  // initialize_memory();
+  // initialize_registers();
 
-  //test for memory
-  store_memory(1,2);
-  store_memory(2, 4);
-  int32_t m = get_memory(1,2);
-  printf("memory test = %d\n", m);
+  // //test for memory
+  // store_memory(1,2);
+  // store_memory(2, 4);
+  // int32_t m = get_memory(1,2);
+  // printf("memory test = %d\n", m);
 
-  //test for registers
-  store_reg(R12, 43);
-  int r = get_reg(R12);
-  printf("register test = %d\n", r);
+  // //test for registers
+  // store_reg(R12, 43);
+  // int r = get_reg(R12);
+  // printf("register test = %d\n", r);
 
-  // //test for read
-  // read_in_binary();
-  // printf("%d\n", get_memory(0, 4));
+  // // //test for read
+  // // read_in_binary();
+  // // printf("%d\n", get_memory(0, 4));
 
-  printf("%d\n", extract_bits(39, 1, 7));
+  // printf("%d\n", extract_bits(39, 1, 7));
   
+  //Test for data processing
+  // int32_t e = 7;
+  // int32_t r = shift(LSR, e, 2, 0);
+  // printf("%d\n", r);
+
+
   //free memory and reg
   free_memory();
   free_registers();
