8ca449e35bec9ded57fe3eab462e0aa9bcf0e1c7
Karim Selih <ks1020@ic.ac.uk>
2021-05-26 14:28:36 +0100

    [ks1020] 80% complete, pending methods from con and pan

--- src/single_data_transfer.h
+++ src/single_data_transfer.h
@@ -1,5 +1,5 @@
 #include <stdint.h>
-# include "registers.h"
+#include "registers.h"
 
 
-void single_data_transfer(int8_t i, int8_t p, int8_t u, int8_t l, enum Register_Names rn, enum Register_Names rd, uint16_t offset)
+void single_data_transfer(int8_t i, int8_t p, int8_t u, int8_t l, enum Register_Names rn, enum Register_Names rd, uint16_t offset);void single_data_transfer(int8_t i, int8_t p, int8_t u, int8_t l, enum Register_Names rn, enum Register_Names rd, uint16_t offset)
