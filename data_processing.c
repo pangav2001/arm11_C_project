5c5f0e12a66fee76903e6e1244611b7a5e6f7445
Konstantinos Koupepas <kk1219@ic.ac.uk>
2021-05-26 16:31:18 +0100

    finished implementig data_processing.c

--- data_processing.c
+++ data_processing.c
@@ -1,28 +1,33 @@
 #include "data_processing.h"
 
-#define set_flag_value(F, V) V ? set_flag(F) : reset_flag(F)
+#define SET_FLAG_VALUE(F, V)             \
+    if (s_flag)                          \
+    {                                    \
+        V ? set_flag(F) : reset_flag(F); \
+    }
 
 #define VALUE_SIZE 32
 
-#define check_c_flag(L)                               \
+#define CHECK_C_FLAG_LOGICAL(L)                       \
     if (amount > VALUE_SIZE - 1 && value != 0)        \
     {                                                 \
-        set_flag(C);                                  \
+        SET_FLAG_VALUE(C, 1);                         \
     }                                                 \
     else                                              \
     {                                                 \
-        set_flag_value(C, extract_bits(value, L, L)); \
+        SET_FLAG_VALUE(C, extract_bits(value, L, L)); \
     }
 
 #define ROTATE_BITS extract_bits(operand2, 8, 11)
-#define IMMEDIATE_VALUE extract_bits(operand2, 0, 7)
+#define IMMEDIATE_VALUE (uint32_t) extract_bits(operand2, 0, 7)
 
 #define RM exctract_bits(operand2, 0, 3)
 #define SHIFT_BY_REGISTER extract_bits(operand2, 4, 4)
 #define REG_SHIFT_TYPE exctract_bits(operand2, 5, 6)
 
 #define RS_LAST_BYTE exctract_bits(operand2, 7, 0)
-#define SHIFT_CONSTANT exctract_bits(operand2, 7, 11)
+#define SHIFT_CONSTANT (uint8_t) exctract_bits(operand2, 7, 11)
+
 
 static enum Shift_Types {
     LSL,
@@ -31,45 +36,99 @@ static enum Shift_Types {
     ROR
 };
 
-static int32_t shift(enum Shift_Types shift_type, int32_t value, int32_t amount)
+static int32_t shift(enum Shift_Types shift_type, int32_t value, int32_t amount, int8_t s_flag)
 {
     switch (shift_type)
     {
     case LSL:
-        check_c_flag(VALUE_SIZE - amount) return value << amount;
+        CHECK_C_FLAG_LOGICAL(VALUE_SIZE - amount)
+        return value << amount;
     case LSR:
-        check_c_flag(amount - 1) return (uint32_t)value >> amount;
+        CHECK_C_FLAG_LOGICAL(amount - 1)
+        return (uint32_t)value >> amount;
     case ASR:
-        check_c_flag(amount - 1) return value >> amount;
+        CHECK_C_FLAG_LOGICAL(amount - 1)
+        return value >> amount;
     case ROR:
         amount *= 2;
         amount %= 32;
         int32_t right = extract_bits(value, 0, amount - 1);
         int32_t left = extract_bits(value, amount, VALUE_SIZE) - 1;
-        check_c_flag(amount - 1) return left + (right << (VALUE_SIZE - amount));
+        CHECK_C_FLAG_LOGICAL(amount - 1)
+        return left + (right << (VALUE_SIZE - amount));
     }
 }
 
-int32_t immediate_operand(int16_t operand2, int8_t i_flag)
+int32_t immediate_operand(int16_t operand2, int8_t i_flag, int8_t s_flag)
 {
     if (i_flag)
     {
-        shift(ROR, ROTATE_BITS, IMMEDIATE_VALUE);
+        shift(ROR, ROTATE_BITS, IMMEDIATE_VALUE, s_flag);
     }
     else if (SHIFT_BY_REGISTER)
     {
         int32_t rs = get_reg(extract_bits(operand2, 8, 11));
-        return shift(REG_SHIFT_TYPE, get_reg(RM), RS_LAST_BYTE);
+        return shift(REG_SHIFT_TYPE, get_reg(RM), RS_LAST_BYTE, s_flag);
     }
     else
     {
-        return shift(REG_SHIFT_TYPE, get_reg(RM), SHIFT_CONSTANT);
+        return shift(REG_SHIFT_TYPE, get_reg(RM), SHIFT_CONSTANT, s_flag);
+    }
+}
+
+static void overflow_check(int32_t a, int32_t b, int32_t result, int8_t s_flag) {
+    if (a > 0 && b > 0 && result < 0 || a < 0 && b < 0 && result > 0) {
+        SET_FLAG_VALUE(C, 1);
+    } else {
+        SET_FLAG_VALUE(C, 0);
     }
 }
 
 void process_func(int8_t i_flag, enum Operators opcode, int8_t s_flag, enum Register_Names rn, enum Register_Names rd, int16_t operand2)
 {
-    int result = 0;
-    set_flag_value(Z, result == 0);
-    set_flag_value(N, exctract_bits(result, 31, 31));
+    int32_t result = 0;
+    int32_t immediate_operand2 = immediate_value(operand2, i_flag);
+
+    switch (opcode)
+    {
+    case AND:
+        result = get_reg(rn) & immediate_operand2;
+        break;
+    case EOR:
+        result = get_reg(rn) ^ immediate_operand2;
+        break;
+    case SUB:
+        result = get_reg(rn) - immediate_operand2;
+        overflow_check(get_reg(rn), -immediate_operand2, result, s_flag);
+        break;
+    case RSB:
+        result = immediate_operand2 - get_reg(rn);
+        overflow_check(get_reg(rn), -immediate_operand2, result, s_flag);
+        break;
+    case ADD:
+        result = get_reg(rn) + immediate_operand2;
+        overflow_check(get_reg(rn), immediate_operand2, result, s_flag);
+        break;
+    case TST:
+        get_reg(rn) & immediate_operand2;
+        break;
+    case TEQ:
+        get_reg(rn) ^ immediate_operand2;
+        break;
+    case CMP:
+        get_reg(rn) - immediate_operand2;
+        overflow_check(get_reg(rn), -immediate_operand2, result, s_flag);
+        break;
+    case ORR:
+        get_reg(rn) | immediate_operand2;
+        break;
+    case MOV:
+        result = immediate_operand2;
+        break;
+    }
+
+    SET_FLAG_VALUE(Z, result == 0);
+    SET_FLAG_VALUE(N, exctract_bits(result, 31, 31));
+
+    store_reg(rd, result);
 }
