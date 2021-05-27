a8c5e6535fd651b9eaf1b8f594152e07e4ae68e2
Naman Sharma <nns20@ic.ac.uk>
2021-05-27 02:22:09 +0100

    fixed branch and decode for branch

--- src/branch.c
+++ src/branch.c
@@ -10,31 +10,10 @@ void branch(int offset){
     //to 32 bits and then added to the PC
 
     //assert(offset != NULL); want to assert 24 bits in length
-    printf("offset input: %d \n", offset);
     int32_t temp = (int32_t) offset << 2;
    
-
-    // error with negative offset
-
-    // check if offset is negative offset
-
-    int first;
-    first = temp;
-
-    while (first >= 10)
-    {
-        first = first / 10;
-    }
-
-    if(first == 1){
-        temp = - temp;
-    }
-
-    printf("offset %d \n", temp);
     // PC is 8 bytes ahead of the instruction that is being executed
     int32_t pc_value = get_reg(PC);
-    printf("pc before %d \n", pc_value);
     pc_value  = (pc_value - 8) + temp;
-    printf("pc after %d \n", pc_value);
     store_reg(PC, pc_value);
 }
