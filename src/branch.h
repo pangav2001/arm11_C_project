04c4bf1fa972539828983cad6752a6d94e893cfb
Karim Selih <ks1020@ic.ac.uk>
2021-06-02 11:15:26 +0100

    Changed dir layout to work on pt 2

--- src/branch.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef BRANCH_H
-#define BRANCH_H
-
-#include <stdint.h>
-
-void branch(int offset);
-
-#endif
--- src/parse_file.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef PARSE_FILE_H
-#define PARSE_FILE_H
-
-#include <stdint.h>
-
-int32_t read_in_binary(char* fileName);
-
-#endif
