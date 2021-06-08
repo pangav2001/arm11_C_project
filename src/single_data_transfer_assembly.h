7c1bfffa972ca435c45bc8f916be655a044e3e50
Naman Sharma <nns20@ic.ac.uk>
2021-06-08 09:53:14 +0100

    fixed header file

--- src/single_data_transfer_assembly.h
+++ src/single_data_transfer_assembly.h
@@ -3,7 +3,10 @@
 #define single_data_transfer_assembly
 
 #include <stdint.h>
+#include "tokenizer.h"
+#include "emulate_src/registers.h"
+#include "emulate_src/memory.h"
 
-int single_data_transfer_assembly(...);
+uint32_t sdt_assembly(enum Mnemonic instruction, enum Register_Names rd, uint16_t address);
 
 #endif
