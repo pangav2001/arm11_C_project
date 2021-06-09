dc75626985ca292c847b136a3dfbcf9054bdf8af
Naman Sharma <nns20@ic.ac.uk>
2021-06-09 09:28:48 +0100

    sdt handles strings & sdt header

--- src/sdt_assembly.c
+++ src/sdt_assembly.c
@@ -3,28 +3,65 @@
 #define COND 0xE // binary 1110 (i.e instruction always happens)
 
 // address - maybe char*
-uint32_t sdt_assembly(enum Mnemonic instruction, enum Register_Names rd, char* address){
-    
+uint32_t sdt_assembly(enum Mnemonic instruction, enum Register_Names rd, char* address,
+int* current_address, int* next_available_address){
+
     // bits 31-28 to cond
     uint32_t result = COND;
 
     //bit 27 - 26 = 01
-    result = (result >> 26) | 1;
-    result = (result >> 27) & 0;
-
-    // bit 15 - 12 has Rd_number
-    result = (result >> )
+    result = result | (1 << 26);
+    result = result & ~(1 << 27);
 
 
-    // given post/pre indexing true or false
-    bool pre_index = true;
+    // bit 15 - 12 has Rd_number -- figure this part out
 
-    // value of expression
-    uint16_t expression = address;
+  
+    uint32_t expression;
 
-    // we can determine which case we are in
-    // if address starts with = then numeric constant
+    // checking for numeric constant case
     bool numeric_constant = false;
+    if(address[0] == '='){
+        numeric_constant = true;
+        expression = strtol(address+3,NULL,16);
+    }
+
+    
+    enum Register_Names Rn = NULL; 
+    bool pre_index = false;
+
+    // check if pre/post index if not numeric constant
+    if(!numeric_constant){
+
+        // determine register 
+        char* rn_pos;
+        rn_pos = strstr(address,"r");
+        rn_pos++;   //since register int is after r
+        Rn = *rn_pos;
+
+        char* hashtag_pos;
+        hashtag_pos = strstr(address,"#");
+    
+        if(hashtag_pos){
+            // to distinguish between [Rn,<#expression>] && [Rn],<#expression>
+            char* closing_bracket;
+            closing_bracket = strstr(address,"]");
+            
+            if(hashtag_pos < closing_bracket){
+                pre_index = true;
+                *closing_bracket = '\0'; //maybe unnecessary but to clearly indicate where number should stop
+            }else{
+                pre_index = false;
+            }
+
+            expression = strtol(hashtag_pos++,NULL,10);
+
+        }else{
+            pre_index = true; // only pre_index has no #
+            expression = 0;
+        }
+    }
+
 
     switch(instruction){
         case LDR:
@@ -33,7 +70,7 @@ uint32_t sdt_assembly(enum Mnemonic instruction, enum Register_Names rd, char* a
 
         //address in form <=expression>
         if(numeric_constant){
-            // if address is just an expression/ numeric constant - then pre indexed
+            // if address is just a numeric constant - then pre indexed
             pre_index = true;
 
             if(expression <= 0xFF){
@@ -50,8 +87,6 @@ uint32_t sdt_assembly(enum Mnemonic instruction, enum Register_Names rd, char* a
         break;
     }
 
-    // deal with Rn
-    enum Register_Names Rn; 
 
     if(pre_index){
         // set the pre-index bit (bit 24)
--- src/sdt_assembly.h
+++ src/sdt_assembly.h
@@ -4,10 +4,13 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 #include "tokenizer.h"
 #include "emulate_src/registers.h"
 #include "emulate_src/memory.h"
 
-uint32_t sdt_assembly(enum Mnemonic instruction, enum Register_Names rd, char* address);
+uint32_t sdt_assembly(enum Mnemonic instruction, enum Register_Names rd,
+char* address,int* current_address, int* next_available_address);
 
 #endif
