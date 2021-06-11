48527588407495232cfb916fa284c4584c9beca6
Karim Selih <ks1020@ic.ac.uk>
2021-06-11 10:52:39 +0100

    [ks1020] Adding addresses to the hash in assemble.c, error with hash keys needs to be fixed

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <stddef.h>
 #include "emulate_src/registers.h"
+#include "hash_table.h"
 
 #define MAX_LINE_LENGTH 511
 #define MAX_OPCODE 4          //I think
@@ -13,7 +14,7 @@ int16_t is_label(char *line);
 
 //@address is the previous address returned by assign_label_address
 // Since we do not increment if the line is a label
-int16_t assign_label_address(char *line, int16_t address);
+int16_t assign_label_address(char *line, int16_t address, Hash_Table *table);
 
 #define DATA_PROCESSING_INDEX 0
 #define MULTIPLY_INDEX 20
