75cabd518b26e61f37fa894b9399bf1aac4a8f81
Konstantinos Koupepas <kk1219@ic.ac.uk>
2021-06-14 21:08:39 +0100

    [kk1219] Renamed types in actors

--- extension/src/actors.h
+++ extension/src/actors.h
@@ -1,35 +1,35 @@
-typedef enum Ghost_Mode {
+typedef enum {
     CHASING,
     CHASED,
     EATEN,
     OFF
-}Ghost_Mode;
+}Ghost_Mode_t;
 
 
-typedef struct pacman{
+typedef struct {
     int x;
     int y;
     int dx;
     int dy;
     char representation;
-}pacman;
+}pacman_t;
 
 
-typedef struct ghost{
+typedef struct {
     int x;
     int y;
     int dx;
     int dy;
     char representation;
-    Ghost_Mode mode;
-}ghost;
+    Ghost_Mode_t mode;
+}ghost_t;
 
-typedef struct game {
+typedef struct {
     int num_ghosts;
-    ghost *ghosts;
-    pacman pacman;
+    ghost_t *ghosts;
+    pacman_t pacman;
     int num_frames_ghost_reset; //-1 means ignore, we wait until 0, then we set ghost to chasing
     int lives;
     int points;
-}game;
+}game_t;
 
