0fec5c933746a0c4ca438cf1f1ceaf13ad06e38d
Karim Selih <ks1020@ic.ac.uk>
2021-06-14 16:11:44 +0100

    [ks1002] skeeleton for actors pacman

---
+++ extension/src/actors.h
@@ -0,0 +1,33 @@
+typedef enum Ghost_Mode {
+    CHASING,
+    CHASED,
+    EATEN,
+    OFF
+}Ghost_Mode;
+
+
+typedef struct pacman{
+    int x;
+    int y;
+    int dx;
+    int dy;
+    char representation;
+}pacman;
+
+
+typedef struct ghost{
+    int x;
+    int y;
+    int dx;
+    int dy;
+    char representation;
+    Ghost_Mode mode;
+}ghost;
+
+typedef struct game {
+    ghost *ghosts;
+    pacman pacman;
+    int lives;
+    int points;
+}game;
+
