8fd31ce99d465f914b83563759d02d0d3ef81c24
Karim Selih <ks1020@ic.ac.uk>
2021-06-15 11:55:15 +0100

    [ks1020] skeleton ghost movement

--- extension/src/ghost_movement.c
+++ extension/src/ghost_movement.c
@@ -1,4 +1,5 @@
 #include "ghost_movement.h"
+#include "game_view.h"
 
 void calculate_ghost_movement(ghost_t *ghost) {
 
@@ -6,4 +7,14 @@ void calculate_ghost_movement(ghost_t *ghost) {
 
 int calculate_target_distance(int curr_x, int curr_y, int target_x, int target_y) {
     return 0;
+}
+
+void move_ghost(game_t *game, char **view) {
+    //update pos
+
+    //update view
+    for (int i = 0; i < game->num_ghosts; i++) {
+        set_character(game->ghosts[i]->x, game->ghosts[i]->y,game->ghosts[i]->representation ,view);
+    }
+
 }
--- extension/src/ghost_movement.h
+++ extension/src/ghost_movement.h
@@ -7,4 +7,5 @@ void calculate_ghost_movement(ghost_t *ghost);
 
 int calculate_target_distance(int curr_x, int curr_y, int target_x, int target_y);
 
+void move_ghost(game_t *game, char **view);
 #endif
