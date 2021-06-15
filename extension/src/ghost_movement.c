82682ce843bbcf90ec5b39f3c726886590fa276d
Karim Selih <ks1020@ic.ac.uk>
2021-06-15 13:42:38 +0100

    [ks1020] added crude ghost movement functionality

--- extension/src/ghost_movement.c
+++ extension/src/ghost_movement.c
@@ -1,20 +1,37 @@
 #include "ghost_movement.h"
 #include "game_view.h"
+#include <stdlib.h>
 
 void calculate_ghost_movement(ghost_t *ghost) {
-
+    //Always chooses the first that makes shorter
+    int curr_distance = calculate_target_distance(ghost->x + ghost->dx, ghost->y + ghost->dy, ghost->target_x, ghost->target_y);
+    int dx = ghost->dx;
+    int dy = ghost->dy;
+    for (int i = -1; i < 2; i++) {
+        for (int j = -1; j < 2; j++) {
+            int new_distance = calculate_target_distance(ghost->x + i, ghost->y + j, ghost->target_x, ghost->target_y);
+            if (new_distance < curr_distance && !(i == 0 && j == 0)) {
+                curr_distance = new_distance;
+                dx = i;
+                dy = j;
+            }
+       
+        }
+    }
+    ghost->dx = dx;
+    ghost->dy = dy;
 }
 
 int calculate_target_distance(int curr_x, int curr_y, int target_x, int target_y) {
-    return 0;
+    return abs(target_x - curr_x) + abs(target_y - curr_y);
 }
 
 void move_ghost(game_t *game, map_t *map) {
     //update pos
-
-    //update view
     for (int i = 0; i < game->num_ghosts; i++) {
-        set_character(game->ghosts[i]->x, game->ghosts[i]->y,game->ghosts[i]->representation ,map);
+        calculate_ghost_movement(GHOST[i]);
+        set_character(GHOST[i]->x, GHOST[i]->y, GHOST[i]->representation ,map);
+        set_character(GHOST[i]->x - GHOST[i]->dx, GHOST[i]->y - GHOST[i]->dy, GHOST[i]->over, map);
     }
 
 }
