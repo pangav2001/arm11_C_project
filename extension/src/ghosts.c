00a1689300856e818a95e277970e461474113244
Karim Selih <ks1020@ic.ac.uk>
2021-06-16 11:35:51 +0100

    [ks1020] Imlemented ghost movement

--- extension/src/ghosts.c
+++ extension/src/ghosts.c
@@ -42,7 +42,24 @@ void init_all_ghosts(game_t *game) {
 }
 
 void update_ghosts_targets(game_t *game) {
-
+    for (int i = 0; i < game->num_ghosts; i++) {
+        switch (GHOSTS[i]->mode) {
+            case OFF:
+                if (GHOSTS[i]->target_x == GHOSTS[i]->x && GHOSTS[i]->target_y == GHOSTS[i]->y) {
+                    GHOSTS[i]->mode = CHASING;
+                }
+                break;
+            case FRIGHTENED:
+                //bit dodgy
+                GHOSTS[i]->target_x = game->map->max_x - PACMAN->x;
+                GHOSTS[i]->target_y = game->map->max_y - PACMAN->y;
+                break;
+            // implement rest
+            default:
+                GHOSTS[i]->target_x = PACMAN->x;
+                GHOSTS[i]->target_y = PACMAN->y;
+        }
+    }
 }
 
 //Checks new movement would be valid
@@ -57,6 +74,80 @@ int diagonal(int dx, int dy) {
     return abs(dx) == 1 && abs(dy) == 1;
 }
 
+int target_distance(int curr_x, int curr_y, int target_x, int target_y) {
+    return abs(target_x - curr_x) + abs(target_y - curr_y);
+
+}
+
+void calculate_ghost_movement(game_t *game, ghost_t *ghost) {
+    int curr_path_distance = target_distance(ghost->x, ghost->y, NEXT_X(ghost), NEXT_Y(ghost));
+
+    //get best movement ignoring curr path
+    //will this always work?
+    int new_dx;
+    int new_dy;
+    int new_distance = game->map->max_x + game->map->max_y;
+    for (int i = -1; i < 2; i++) {
+        for (int j = -1; j < 2; j++) {
+            int distance = target_distance(ghost->x + i, ghost->y + j, ghost->target_x, ghost->target_y);
+            //stop ghost going over eachother
+            if (distance < new_distance && !diagonal(i,j) && (i != 0 || j != 0) && check_ghost_position(game, ghost, i, j)) {
+                new_dx = i;
+                new_dy = j;
+                new_distance = distance;
+            }
+        }
+    }
+
+    //choose current path or change
+    if (curr_path_distance < new_distance && check_ghost_position(game, ghost, ghost->dx, ghost->dy)) {
+        new_dx = ghost->dx;
+        new_dy = ghost->dy;
+    }
+
+    //Case where we want to stay maybe set new_dx to -2 check if -2 then set to 0
+
+    char in_way = get_char(ghost->x + new_dx, ghost->y + new_dy, game->map);
+
+    switch (in_way) {
+        case '#':
+        case 'G':
+            ghost->dx = 0;
+            ghost->dy = 0;
+            break;
+        case '-':
+            if (!(ghost->mode == OFF || ghost->mode == EATEN))
+            {
+                ghost->dx = 0;
+                ghost->dy = 0;
+                break;
+            }
+        default:
+            ghost->dx = new_dx;
+            ghost->dy = new_dy;
+            ghost->x = NEXT_X(ghost);
+            ghost->y = NEXT_Y(ghost);
+    }
+}
+
 void move_ghosts(game_t *game) {
+    for (int i = 0; i < game->num_ghosts; i++) {
+        if (GHOSTS[i]->ghost_wait == 0) {
+            GHOSTS[i]->ghost_wait = GHOST_WAIT;
+
+            calculate_ghost_movement(game, GHOSTS[i]);
+
+            if (!NOT_MOVING(GHOSTS[i])) {
+                set_char(PREV_X(GHOSTS[i]), PREV_Y(GHOSTS[i]), GHOSTS[i]->over, game->map);
+                GHOSTS[i]->over = get_char(GHOSTS[i]->x, GHOSTS[i]->y, game->map);
+                set_char(GHOSTS[i]->x, GHOSTS[i]->y, GHOST_REPRESENTATION, game->map);
+            }
+        } else {
+            GHOSTS[i]->ghost_wait --;            
+        }
+
+
+    }
+
 
 }
