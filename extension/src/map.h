81b7ed0528e54068c220fb8172a97c24382218c9
Karim Selih <ks1020@ic.ac.uk>
2021-06-16 22:12:04 +0100

    [ks1020] Game over screen has correct colours

--- extension/src/map.h
+++ extension/src/map.h
@@ -5,6 +5,8 @@
 
 #include <ncurses.h>
 
+void print_char_colour(Game_Constructs_t object, int x, int y, char c, int frightened_mode);
+
 void print_view(WINDOW *window, game_t *game);
 
 void set_char(int x, int y, char ch, map_t *map);
