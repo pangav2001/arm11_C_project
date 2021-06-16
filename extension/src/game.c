91ac22abab945418f2db72c9100fd3c7af6b58fd
Konstantinos Koupepas <kk1219@ic.ac.uk>
2021-06-16 11:33:41 +0100

    [kk1219] Implemented game.c

---
+++ extension/src/game.c
@@ -0,0 +1,148 @@
+#include "game.h"
+#include <ncurses.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define DELAY 50000
+
+game_t *create_game()
+{
+    game_t *ret = (game_t *)malloc(sizeof(game_t));
+
+    create_ghosts(ret);
+    create_pacman(ret);
+    create_map(ret);
+
+    return ret;
+}
+
+void free_game(game_t *game)
+{
+    free_pacman(PACMAN);
+    free_ghosts(GHOSTS);
+    free_map(MAP);
+
+    free(game);
+}
+
+void init_game(game_t *game)
+{
+    game->lives = 3;
+    game->num_frames_ghost_reset = -1;
+
+    game->num_ghosts = 1;
+    game->points = 0;
+    game->high_score = 10700;
+
+    init_all_ghosts(GHOSTS);
+    init_pacman(PACMAN);
+    init_map(MAP);
+}
+
+void print_game_over()
+{
+    int max_y;
+    int max_x;
+    getmaxyx(stdscr, max_y, max_x);
+
+    for (int i = max_x; i >= 0; i--)
+    {
+        clear();
+
+        int len = strlen(" _____   ___  ___  ___ _____   _____  _   _ ___________ ") / 2;
+
+        //Change 'G'
+        mvprintw(max_y / 2, i, "%c  %c %c %c %c", PACMAN_REPRESENTATION, 'G', 'G', 'G', 'G');
+        mvprintw(max_y / 2 + 1, max_x / 2 - len, " _____   ___  ___  ___ _____   _____  _   _ ___________ ");
+        mvprintw(max_y / 2 + 2, max_x / 2 - len, "|  __ \\ / _ \\ |  \\/  ||  ___| |  _  || | | |  ___| ___ \\");
+        mvprintw(max_y / 2 + 3, max_x / 2 - len, "| |  \\// /_\\ \\| .  . || |__   | | | || | | | |__ | |_/ /");
+        mvprintw(max_y / 2 + 4, max_x / 2 - len, "| | __ |  _  || |\\/| ||  __|  | | | || | | |  __||    / ");
+        mvprintw(max_y / 2 + 5, max_x / 2 - len, "| |_\\ \\| | | || |  | || |___  \\ \\_/ /\\ \\_/ / |___| |\\ \\ ");
+        mvprintw(max_y / 2 + 6, max_x / 2 - len, " \\____/\\_| |_/\\_|  |_/\\____/   \\___/  \\___/\\____/\\_| \\_|");
+
+        refresh();
+
+        usleep(DELAY);
+    }
+}
+
+int main(void)
+{
+    // Setup window
+
+    initscr();
+    nodelay(stdscr, true);
+    cbreak();
+    noecho();
+    curs_set(FALSE);
+
+    game_t *game = create_game();
+
+    init_game(game);
+
+    int h = 64;
+    int w = 64;
+
+    WINDOW *window = newwin(h, w, 0, 0);
+
+    while (!game_over(game))
+    {
+        char input = getch();
+
+        int dx = PACMAN->dx;
+        int dy = PACMAN->dy;
+
+        switch (input)
+        {
+        case 'w':
+            dy = -1;
+            dx = 0;
+
+            break;
+        case 'a':
+            dy = 0;
+            dx = -1;
+            break;
+        case 's':
+            dy = 1;
+            dx = 0;
+
+            break;
+        case 'd':
+            dy = 0;
+            dx = 1;
+            break;
+        }
+        flushinp();
+
+        move_pacman(game, dx, dy);
+        move_ghost(game, MAP);
+
+        if (MAP->pellet_num <= 0)
+        {
+            init_all_ghosts(GHOSTS);
+            init_pacman(PACMAN);
+            init_map(MAP);
+        }
+
+        update_ghost_targets(game, MAP);
+
+        //update everything else
+        update_view(game, MAP);
+
+        //rerender screen
+        print_view(window, game);
+    }
+
+    print_game_over();
+
+    endwin();
+
+    free_game(game);
+}
+
+int game_over(game_t *game)
+{
+    return game->lives <= 0;
+}
