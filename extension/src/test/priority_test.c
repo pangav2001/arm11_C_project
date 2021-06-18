a2ce17f2ed992bac79f0523bb13a94c193c595bf
Karim Selih <ks1020@ic.ac.uk>
2021-06-18 11:33:21 +0100

    [ks1020] added skeleton tests for pq

---
+++ extension/src/test/priority_test.c
@@ -0,0 +1,10 @@
+#include <testutils.h>
+#include <../priority_queue.h>
+
+
+
+int main(void) {
+
+    Node **PQ = new_queue();
+}
+
---
+++ extension/src/test/testutils.h
@@ -0,0 +1,8 @@
+#include <stdbool.h>
+
+extern void testcond( bool condition, char * testname );
+extern void testint( int got, int expected, char * testname );
+extern void testchar( char got, char expected, char * testname );
+extern void testlong( long got, long expected, char * testname );
+extern void testdouble( double got, double expected, char * testname );
+extern void teststring( char * got, char * expected, char * testname );
