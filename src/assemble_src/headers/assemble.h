74a66b80cfaf10a2155c4063cb031fe9bf2fe021
Konstantinos Koupepas <kk1219@ic.ac.uk>
2021-06-18 13:02:34 +0100

    [kk1219] Changed read_in_prog to use uint8

--- src/assemble_src/headers/assemble.h
+++ src/assemble_src/headers/assemble.h
@@ -5,6 +5,6 @@
 
 void save_instruction(uint32_t *assembled_program, uint16_t address ,uint32_t data);
 
-char **read_in_prog(char *filename, int *num_instr);
+char **read_in_prog(char *filename, uint8_t *num_instr);
 
 #endif
